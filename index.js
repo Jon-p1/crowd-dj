5218f6fd8a12d7a1ade396b4914d28ead8dc5dd5
jonathan <jonathan@crec.us>
2019-09-10 18:13:38 -0700

    postgres connection

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const Pool = require('pg').Pool;
+
 var PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+
+
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'crowd-dj',
+  password: '',
+  post: 5432
+});
+
+// const queryString = "COPY hubspot_data(dealname,property_address,listing_agent,property_type_of_use,buyer_seller_or_investor) FROM 'D:\\work\\heroku-postgres\\hubspot-deal-data.csv' DELIMITER ',' CSV HEADER";
+const queryString = "INSERT INTO events(event_name, dj_name, size) VALUES('2019 House party', 'dj spike', 1)";
+pool.query(queryString,(error, results) => {
+	if(error) throw error;
+	else console.log(results);
+	pool.end();
+})
+
 
 app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname+'/index.html'));
