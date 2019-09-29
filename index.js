b102957312c8f86166b8953d11121403308551fc
jonathan <jonathan@crec.us>
2019-09-28 17:31:43 -0700

    join route updated

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 require('dotenv').config()
 const db = require('./queries.js');
+const bodyParser = require('body-parser');
+const path = require('path');
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
 const Pool = require('pg').Pool;
 
 var PORT = process.env.PORT || 3000;
@@ -15,6 +16,10 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname+'/index.html'));
 });
+
+app.get('/event', (req, res) => {
+	res.sendFile(path.join(__dirname+'/public/event.html'));
+})
 
 app.get('/events', db.getEvents)
 app.get('/events/:id', db.getEventById) 
